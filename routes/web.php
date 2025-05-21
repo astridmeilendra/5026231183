ee33046a526e77affbf13a69c235b1b589463c71
astridmeilendra <astridmeilendra91@gmail.com>
2025-05-21 12:38:42 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('index', function () {
     return view('index');
 });
+
 Route::get('tugaslinktree', function () {
     return view('tugaslinktree');
 });
+
 Route::get('bootstrap1', function () {
     return view('bootstrap1');
 });
+
 Route::get('js1', function () {
     return view('js1');
 });
@@ -33,6 +29,7 @@ Route::get('js1', function () {
 Route::get('js2', function () {
     return view('js2');
 });
+
 Route::get('pertama', function () {
     return view('pertama');
 });
@@ -45,23 +42,24 @@ Route::get('frontend', function () {
     return view('frontend');
 });
 
-
-=======
-//import java.io;
-
-//
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('halo', function () {
-    return "<h2>Halo, Selamat datang di Laravel! www.malasngoding.com</h2>";
+    return "<h2> Halo, Selamat datang di Laravel! www.malasngoding.com</h2>";
 });
 
 Route::get('blog', function () {
     return view('blog');
 });
 
-Route::get('hello', [Link::class, 'blog'])->name('helloworld'); {
-};
->>>>>>> bc3768d0e5cff03fb43878cfc3c1306c7acaeee9
+Route::get('hello', [LinkController::class, 'blog'])->name('helloworld');
+
+Route::get('dosen', [Link::class,'index']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
